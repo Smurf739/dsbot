994454ed2abb53fddda080176e7699d6c30d7574
Smurf739 <dd.chelnik@gmail.com>
2024-04-10 13:29:31 +0300

    запись в бд
    при создании голосового канала создаётся запись в бд, а при выходе удаляется

--- cogs/private_rooms.py
+++ cogs/private_rooms.py
@@ -1,5 +1,8 @@
+import datetime
+
 import disnake
 from disnake.ext import commands
+import database
 
 
 mychannel = None
@@ -17,11 +20,20 @@ class UserHandler(commands.Cog):
                 mychannel = await after.channel.category.create_voice_channel(
                     name=member.name
                 )
+                date_created = datetime.datetime.now().strftime(
+                    "%y years %m month %d days %H hours %M minutes %S seconds")
+                print(type(date_created))
+                print(date_created)
                 await member.move_to(mychannel)
+                await database.create_room_info(voice_i=mychannel.id,
+                                                voice_n=mychannel.name,
+                                                date=date_created,
+                                                owner=member.id)
 
         except AttributeError:
             if before.channel == mychannel:
                 if len(before.channel.members) == 0:
+                    await database.remove_room(before.channel.id)
                     await before.channel.delete()
             else:
                 pass
--- database.py
+++ database.py
@@ -6,12 +6,27 @@ async def crate_table():
         await db.execute("CREATE TABLE IF NOT EXISTS rooms_info ("
                          "voice_id INTEGER PRIMARY KEY AUTOINCREMENT,"
                          "voice_name varchar(255) NOT NULL,"
-                         "date_created varchar(255) NOT NULL,"
-                         "limit_user INTEGER NOT NULL,"
-                         "invisible BOOL NOT NULL,"
-                         "is_closed BOOL NOT NULL,"
+                         "date_created TEXT NOT NULL,"
+                         "limit_user INTEGER DEFAULT 0,"
+                         "invisible BOOL DEFAULT FALSE,"
+                         "is_closed BOOL DEFAULT FALSE,"
                          "owner_id INTEGER NOT NULL,"
-                         "coadministrator_id INTEGER NOT NULL)")
+                         "coadministrator_id INTEGER DEFAULT 0)")
+        await db.commit()
+
+
+async def create_room_info(voice_i, voice_n, date, owner):
+    async with aiosqlite.connect("rooms.db") as db:
+        await db.execute(f"INSERT INTO rooms_info "
+                         f"(voice_id, voice_name, date_created, owner_id)"
+                         f" VALUES ('{voice_i}',"
+                         f" '{voice_n}', '{date}', '{owner}')")
+        await db.commit()
+
+
+async def remove_room(voice_i):
+    async with aiosqlite.connect("rooms.db") as db:
+        await db.execute(f"DELETE FROM rooms_info WHERE voice_id = '{voice_i}'")
         await db.commit()
 
 
